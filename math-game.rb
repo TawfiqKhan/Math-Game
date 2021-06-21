c32c350e01edacafcf23b3ea66956ab618d68cd9
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-21 17:02:26 -0400

    initial planning

---
+++ math-game.rb
@@ -0,0 +1,33 @@
+# Ask Question
+# Get answer
+# compare the answer with the correct answer
+# display the result
+
+# Start another round
+
+# control player creation
+# Player class
+  # player name
+  # lives
+
+# player class will habdle player creation and will have control over player lives
+
+# This will control game logic
+# Game class
+# methods
+  # ask question
+  # compare answer
+  # show result
+
+  # class Player
+
+  # end
+
+  # class Game
+  #   def createGame
+  #     player1 = Player.new("1", 0)
+  #     player2 = Player.new("2")
+  #   end
+  # end
+
+  # game1 = Game.new()
