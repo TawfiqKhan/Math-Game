932d5c027eac757e65137acd2fece1982487d954
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-21 18:58:01 -0400

    adding methods to check answer

--- main.rb
+++ main.rb
@@ -23,21 +23,23 @@ puts q1.question
 a1 = Input.new()
 # puts a1.check_answer(q1.answer) ? "Correct" : "Incorrect"
 
-def game_status (p1, p2, question, user_answer)
-  if(!user_answer.check_answer(question.answer))
-    puts "Current life #{p1.remaining_lives}"
-    p1.lose_life()
-    puts "You only have #{p1.remaining_lives} live remaining"
+def compare_answer (player, question, player_answer)
+
+  if(!player_answer.check_answer(question.answer))
+    # puts "Current life #{player.remaining_lives}"
+    puts "#{player.name}: Seriously? No! Correct answer is #{question.answer}"
+    player.lose_life()
+    return false
+    # puts "You only have #{p1.remaining_lives} live remaining"
   else 
-    puts "p1: #{p1.remaining_lives}/3 vs p1: #{p2.remaining_lives}/3"
+    # puts "p1: #{p1.remaining_lives}/3 vs p1: #{p2.remaining_lives}/3"
+    return false
   end
 end
 
-game_status(game1.player1, game1.player2, q1, a1)
-# if(!a1.check_answer(q1.answer))
-#   puts "Current life #{game1.player1.remaining_lives}"
-#   game1.player1.lose_life()
-#   puts "You only have #{game1.player1.remaining_lives} live remaining"
-# else 
-#   puts "p1: #{game1.player1.remaining_lives}/3 vs p1: #{game1.player2.remaining_lives}/3"
-# end
+def game_status (p1, p2)
+  puts "p1: #{p1.remaining_lives}/3 vs p1: #{p2.remaining_lives}/3"
+end
+
+compare_answer(game1.player1, q1, a1)
+game_status(game1.player1, game1.player2)
